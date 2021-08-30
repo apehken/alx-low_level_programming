b0187d40b0be1e6d80e9523c4d947b281d3ea93a
Kenneth Apeh <apehken@gmail.com>
2021-08-30 11:30:59 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - Print all single digit numbers
+* main - Print all single digit numbers using only putchar
 *
 * Return: Always 0 (Success)
 */
@@ -10,7 +10,8 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
-	printf("%i", digit);
+	putchar(digit + '0');
+
 	putchar('\n');
 
 	return (0);
